93d0dff7f608a70c8926e6a14c8ac5ce2de89d57
Ayoub Mazzouz <ayoub.mazzouz@etu.uae.ac.ma>
2024-07-15 21:31:02 +0100

    first version

---
+++ src/main/java/com/summer/springsummer/SpringSummerApplication.java
@@ -0,0 +1,13 @@
+package com.summer.springsummer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSummerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSummerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/summer/springsummer/SpringSummerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summer.springsummer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSummerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
